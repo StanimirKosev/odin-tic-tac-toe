60a0cdb5ada02a8c5b1620af126d554f0411635b
Stanimir <stanimirkosev00@gmail.com>
2022-04-24 14:52:58 +0300

    css done

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let gameBoard = (() => { /** Module Pattern */
             let table = document.querySelector('.table');
             let el = document.createElement('button');
             table.appendChild(el).className = 'el';
+            el.setAttribute['data-el-index',i];
             el.innerText = _board[i];
         }
     }
@@ -22,20 +23,34 @@ let gameBoard = (() => { /** Module Pattern */
     }
 
 })();
-
 gameBoard.gameTable();
 
 
+let players = (name,symbol) => { /** Factory function */
+    return {name,symbol};
+};
+let player1 = players('player1','X');
+let player2 = players('player2','O');
 
 
 let displayController = (() => {
     'use strict';
 
+    document.addEventListener('click', (event) => {
+        plays(event);
+    })
+
+    function plays(event){
+        const index = event.target.getAttribute('data-el-index');
+        const el = document.querySelectorAll('[data-el-index]')[index];
+        
+        if (players.name === 'player1'){
+            el.innerText = player1.symbol;
+        }
+        else if (players.name === 'player2'){
+            el.innerText = player2.symbol;
+        }
+    }
+    
 })();
-
-let players = (name,symbol) => { /** Factory function */
-    return {name,symbol};
-
-};
-let player1 = players('player1','O');
-let player2 = players('player2','X');
+/** trq izkaram eventlistenera posle i kak da svurjesh inner txt...playerite purvo opravi*/
